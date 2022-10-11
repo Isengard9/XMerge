d914aadda8621f99979ca46a2ebdf45afc705d8d
Nurullah Çelik <66533548+ogrnurullahcelik@users.noreply.github.com>
2022-10-11 23:40:53 +0300

    Update: Raycast to grid parts

---
+++ Assets/Scripts/Controllers/RayCastController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Game.General.Systems;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Controllers
+{
+    public class RayCastController : MonoBehaviour
+    {
+        private RaycastHit hit;
+
+        [SerializeField] private LayerMask gridMask;
+
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                RayToGrid();
+            }
+        }
+
+        #region Control Check Grid Hitted
+
+        private void RayToGrid()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, gridMask))
+            {
+                var gridPart = hit.transform.GetComponent<GridPart>();
+                gridPart.SetFull();
+            }
+        }
+
+        #endregion
+    }
+}
+
